c0e504f3a40698df3410e22d1c359a26417f68da
iecetitiz <i.ecetitiz@gmail.com>
2022-12-03 22:29:17 +0300

    constructor and class methods are added

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -9,24 +9,39 @@ public: //by default private
     string OwnerName;
     int SubscribersCount;
     list<string> PublishedVideoTitles;
+
+    YouTubeChannel(string name, string ownerName) {
+        Name = name;
+        OwnerName = ownerName;
+        SubscribersCount = 0;
+    }
+
+    void GetInfo() { //i will run this codes in the object class instead main method
+    cout << "Name:" << Name<<endl;
+    cout << "Owner Name:" << OwnerName<<endl;
+    cout << "Subscriber count:" << SubscribersCount<<endl;
+    cout << "Videos:"<<endl;
+    for(string VideoTitle: PublishedVideoTitles) {
+        cout << VideoTitle<<endl;
+    }
+
+    }
 };
 
 int main()
 {
-    YouTubeChannel ytChannel;
-    ytChannel.Name = "CodeBeauty";
-    ytChannel.OwnerName = "Saldina";
-    ytChannel.SubscribersCount = 1800;
-    ytChannel.PublishedVideoTitles = {"C++ for beginners", "html&css", "C++ OOP"};
+    YouTubeChannel ytChannel("CodeBeauty", "Saldina"); //icini bos birakamam
+    ytChannel.PublishedVideoTitles.push_back("C++ for beginners");
+    ytChannel.PublishedVideoTitles.push_back("html&css");
+    ytChannel.PublishedVideoTitles.push_back("C++ OOP");
+    ytChannel.GetInfo();
+    //ytChannel.Name = "CodeBeauty";
+    //ytChannel.OwnerName = "Saldina";
+    //ytChannel.SubscribersCount = 1800;
+    //ytChannel.PublishedVideoTitles = {"C++ for beginners", "html&css", "C++ OOP"};
 
 
-    cout << "Name:" << ytChannel.Name<<endl;
-    cout << "Owner Name:" << ytChannel.OwnerName<<endl;
-    cout << "Subscriber count:" << ytChannel.SubscribersCount<<endl;
-    cout << "Videos:"<<endl;
-    for(string VideoTitle: ytChannel.PublishedVideoTitles) {
-        cout << VideoTitle<<endl;
-    }
+
 
 
 
